5b1c1cad95be4e967471256a6b6cc28f9f5241e1
kaviyasrinivasan <kaviyasrini2004@gmail.com>
2024-11-15 21:23:41 +0530

    Initial commit

---
+++ kaviya's_boutique/backend/data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: "Green Lehenga",
+    image: "/images/anarkali2.jpg",
+    description:
+      "Floral Women Polyester Straight Kurta",
+    brand: "Biba",
+    category: "Lehenga",
+    price: 2500.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Floral Women Polyester Straight Kurta",
+    image: "/images/kurta1.jpg",
+    description:
+      "Floral Women Polyester Straight Kurta",
+    brand: "Levi's",
+    category: "Kurta",
+    price: 1200.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Women's Rayon Anarkali Kurta with Pant & Dupatta",
+    image: "/images/anarkali.webp",
+    description:"Women's Rayon Anarkali Kurta with Pant & Dupatta",    brand: "Adidas",
+    category: "Anarkali",
+    price: 900.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Women's Solid & Embroidered Rayon Anarkali Kurta with Pant & Dupatta",
+    image: "/images/anarkali1.jpg",
+    description:
+      "Women's Solid & Embroidered Rayon Anarkali Kurta with Pant & Dupatta",
+    brand: "Libas",
+    category: "Anarkali",
+    price: 4900.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Women's Georgette Anarkali Floral Printed Kurta with Pant & Dupatta",
+    image: "/images/chudi.jpg",
+    description:
+      "Women's Georgette Anarkali Floral Printed Kurta with Pant & Dupatta",
+    brand: "Biba",
+    category: "Anarkali",
+    price: 1500.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Women's Cotton Silk Jacquard Kurta Pant with Dupatta Set",
+    image: "/images/chudi1.jpg",
+    description:
+      "Maroon Kurta set",
+    brand: "Fusion",
+    category: "Kurta",
+    price: 3500.00,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
